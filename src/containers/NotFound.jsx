646c8d81ac96fa47ddccd841595a8cabf6267eda
Paul Cortes <paulkerg@gmail.com>
2020-12-11 21:55:54 -0500

    Agregado componente de no encontrado

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>No Encontrado</h1>
+  </>
+);
+
+export default NotFound;
