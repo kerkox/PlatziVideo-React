d256644f99f7fc38c5961924d4c885ff66497f5f
Paul Cortes <paulkerg@gmail.com>
2020-12-13 11:32:25 -0500

    Validacion si ya esta agregado no agregar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,13 @@ const reducer = (state, action) => {
 
   switch (action.type) {
     case 'SET_FAVORITE':
-      return {
-        ...state,
-        myList: [...state.myList, action.payload],
-      };
+      if (state.myList.every((item) => item.id !== action.payload.id)) {
+        return {
+          ...state,
+          myList: [...state.myList, action.payload],
+        };
+      }
+      return state;
 
     case 'DELETE_FAVORITE':
       return {
