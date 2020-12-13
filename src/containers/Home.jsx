0073c41e6566fed40b30a8387b90cd5dabbd79a0
Paul Cortes <paulkerg@gmail.com>
2020-12-12 23:40:41 -0500

    implementado los reducers

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,22 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-  const initialState = useInitialState(API);
-
+const Home = ({ myList, trends, originals }) => {
   return (
     <>
       <Search />
       {
-        initialState.mylist.length > 0 && (
+        myList.length > 0 && (
           <Categories title='Mi Lista'>
             <Carousel>
               <CarouselItem />
@@ -28,7 +24,7 @@ const Home = () => {
       <Categories title='Tendencias'>
         <Carousel>
           {
-            initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+            trends.map((item) => <CarouselItem key={item.id} {...item} />)
           }
         </Carousel>
       </Categories>
@@ -36,7 +32,7 @@ const Home = () => {
       <Categories title='Originales'>
         <Carousel>
           {
-            initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+            originals.map((item) => <CarouselItem key={item.id} {...item} />)
           }
 
         </Carousel>
@@ -46,4 +42,11 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+export default connect(mapStateToProps, null)(Home);
