2bd8bdb22a9a7e957985c7ff3367b29c11f5ff42
Paul Cortes <paulkerg@gmail.com>
2021-01-09 23:53:20 -0500

    Rutas desde el servidor

---
+++ src/frontend/routes/serverRoutes.js
@@ -0,0 +1,35 @@
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import NotFound from '../containers/NotFound';
+import Player from '../containers/Player';
+import Register from '../containers/Register';
+
+const routes = [
+  {
+    exact: true,
+    path:'/',
+    component:  Home
+  },
+  {
+    exact: true,
+    path:'/login',
+    component:  Login
+  },
+  
+  {
+    exact: true,
+    path:'/register',
+    component:  Register
+  },
+  {
+    exact: true,
+    path:'/player/:id',
+    component:  Player
+  },
+  {
+    name: 'NotFound',
+    component: NotFound
+  },
+];
+
+export default routes;
