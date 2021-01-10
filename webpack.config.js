8319e795f290cdd27b99e26387171aec79c6515b
Paul Cortes <paulkerg@gmail.com>
2021-01-10 01:59:45 -0500

    avances en la compresion de los archivos

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
 require('dotenv').config();
 
 const isDev = (process.env.ENV == 'development');
@@ -21,6 +24,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   module: {
     rules: [
       {
@@ -28,12 +35,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
         },
       },
       {
@@ -64,6 +65,11 @@ module.exports = {
   },
   plugins: [
     isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+    isDev ? () => {} : 
+    new CompressionWebpackPlugin({
+      test: /\.js$|\.css$/,
+      filename: '[path][base].gz'
+    }),
     new MiniCssExtractPlugin({
       filename: 'assets/app.css',
     }),
